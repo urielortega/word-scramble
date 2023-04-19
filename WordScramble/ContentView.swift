474ea45deeb0b786d815dbaeb7b33d02ef813e70
Uriel Ortega <urielortega2509@gmail.com>
2023-04-19 17:26:13 -0600

    Initial Commit

---
+++ WordScramble/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  WordScramble
+//
+//  Created by Uriel Ortega on 19/04/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
